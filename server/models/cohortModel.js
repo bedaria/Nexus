bb39eab9e520babe0558378847a6215e5bc3fd3a
Michael <michaelhuytran@gmail.com>
2016-10-06 12:40:34 -0700

    [feature] -adds CohortModel

--- server/models/cohortModel.js
+++ server/models/cohortModel.js
@@ -1,2 +1,9 @@
 const Sequelize = require('sequelize');
 const connection = require('../config/db');
+
+const Cohort = connection.define('user', {
+  cohort: {
+    type: Sequelize.INTEGER
+}
+
+module.exports = Cohort;
