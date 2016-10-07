6c0a8b30b3362be5249673b3aa66aa3881e994a1
Sam Chi <samuel.sj.chi@gmail.com>
2016-10-06 20:15:27 -0700

    [pull] - creates TodoService for managing todo items, implements addTodo method

---
+++ src/app/todo-list/todo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+
+@Injectable()
+export class TodoService {
+  // Placeholder for last id to simulate automatic incrementing of id's
+  lastId: number = 0;
+
+  // Temporarily store todo's in memory
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  // Temporary placeholder for POST /api/users/:userId/todos
+  addTodo(todo: Todo): TodoService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+}
