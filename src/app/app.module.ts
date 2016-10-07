623968188dbed02cb3da03cb9ff557f1aab8566f
Sam Chi <samuel.sj.chi@gmail.com>
2016-10-06 20:35:57 -0700

    [pull] - creates TodoListComponent and sets up component's view in html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {Todo} from './todo';
+import {TodoService} from './todo.service';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+  providers: [TodoService]
+})
+export class TodoListComponent {
+  newTodo: Todo = new Todo();
+
+  constructor(private todoService: TodoService) { }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+}
