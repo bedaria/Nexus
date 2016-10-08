ccaa2bf03c0d6a1f872df3e5abead7a5786f3bf7
Michael <michaelhuytran@gmail.com>
2016-10-08 12:21:52 -0700

    [feature] - adds user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,14 +3,62 @@
  */
 const model = require('.../models/userModel.js');
 
+let uniqueIdentifier;
+
 module.exports = {
-  signup: (req, res) => {}
+  signup: (req, res) => {
+    const newUser = models.User.build({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      cohort: req.body.cohort,
+      profilePic: req.body.profilePic,
+      bio: req.body.bio,
+    });
 
-  signin: (req, res) => {}
+    newUser.save() //saves to database
+      .then(inputs => {
+        //will hash password here
+        password.hash(req.body.password)
+        .then(hashedPassword => {
+          newUser.update({ password: hashedPassword });
+          //create webtoken
+        })
+      .catch(err => console.log("Error while hashing password, ", err))
+      })
+      .catch(error => {
+        console.log("Error while creating new User ", error);
+        res.status(500).send(err);
+      });
+  },
+
+  signin: (req, res) => {
+    if((!req.body.username && !req.body.email) || !req.body.password){
+      res.status(500).send("Invalid or missing inputs");
+    }
+    uniqueIdentifier = (req.body.username) ? 'username' : 'email';
+    model.findOne({
+      where:{
+        uniqueIdentifier : req.body[uniqueIdentifier]
+      },
+      attributes: ['id', 'firstName', 'lastName', 'email', 'username', 'password', 'profilePic', 'bio']
+    })
+      .then( foundUser => {
+        if(!foundUser) res.status(500).send('User not found.');
+        else {
+          //compare userPassword
+            .then( successfulMatch => {
+
+          })
+            .catch( err => {
+              console.log('Error:', err);
+              res.status(500).send("Password do not match", err);
+            })
+        }
+      })
+  },
+
 }
-  //receive the axios call and store them into the website
-
-// userController.signup
-// userController.signin
-// reauthenticate
 
