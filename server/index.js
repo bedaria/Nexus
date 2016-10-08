de87dc69cd75eec6523f50585bb53d0dd89bba8b
bedaria <dburinsk@gmail.com>
2016-10-08 15:24:22 -0700

    [fix] - deletes unnecessary route

--- server/index.js
+++ server/index.js
@@ -10,13 +10,7 @@ require('./models/models.js');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.get('/api/hello', (req, resp) => {
-  resp.send("hellor")
-});
-
 io.on('connection', socket => {
-  console.log("user connected")
-  socket.on('disconnect', () => console.log("user disconnected"));
   socket.on('announce', (announcementInfo) => io.emit('notification', announcementInfo));
 });
 
