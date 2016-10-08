0ac8683d7e2658de55d54ff118de9cd3921bba23
bedaria <dburinsk@gmail.com>
2016-10-08 15:24:57 -0700

    [pull] - puts notifications folder files into announcements folder

---
+++ src/app/announcements/notifications.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import * as io from 'socket.io-client';
+
+@Component({
+  selector: 'app-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.css']
+})
+
+export class NotificationComponent {
+  notification = '';
+  cohort = '';
+  showNotification = false;
+  socket = io('http://localhost:3000');
+
+  receiveNotification = () => {
+    this.socket.on('notification', (notification) => {
+      this.notification = notification.message
+      this.showNotification = true;
+      this.cohort = notification.cohort;
+    })
+    setTimeout(()=> this.showNotification = false, 10000);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AnnouncementsComponent } from './announcements/announcements.component';
-import { NotificationComponent } from './notifications/notifications.component';
+import { NotificationComponent } from './announcements/notifications.component';
 import { LoginComponent } from './login/login.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { SignupComponent } from './signup/signup.component';
