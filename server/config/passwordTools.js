789830716f40349c888b25f751a60f44b4322dfc
Michael <michaelhuytran@gmail.com>
2016-10-08 13:03:14 -0700

    [feature] - password encryption

--- server/config/passwordTools.js
+++ server/config/passwordTools.js
@@ -1,3 +1,30 @@
-/**
- * Created by MikeTran on 10/8/16.
- */
+const pbkdf2 = require('pbkdf2');
+let salt = crypto.randomBytes(128).toString('base64');
+
+hash = param => {
+  return new Promise(function(resolve, reject) {
+    crypto.pbkdf2(param, salt, 10000, 512, function(error, hash) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(hash);
+      }
+    });
+  });
+};
+
+checkPassword = (attemptedPass, hash) => {
+  return new Promise(function(resolve, reject) {
+    if (hash(attemptedPass) == hash){
+      resolve(result);
+    }
+    reject(error);
+  });
+};
+
+
+
+module.exports = {
+  hash,
+  checkPassword
+}
