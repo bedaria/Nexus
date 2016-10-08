57504c41b97ec70496fdb1c08fd126976bf4e2b5
bedaria <dburinsk@gmail.com>
2016-10-08 13:14:04 -0700

    [fix] - fixes socket event names

--- server/index.js
+++ server/index.js
@@ -7,25 +7,19 @@ const db         = require('./config/db.js');
 const app        = express();
 require('./models/models.js');
 
-
-const express = require('express');
-const port = process.env.PORT || 4200;
-const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.get('/api/hello', (req, resp) => {
   resp.send("hellor")
-})
+});
 
 io.on('connection', socket => {
   console.log("user connected")
-
   socket.on('disconnect', () => console.log("user disconnected"));
-  socket.on('announce', (msg) => {
-    console.log("received message! ", msg)
-    io.emit('a', msg)
-  })
+  socket.on('announce', (announcementInfo) => io.emit('notification', announcementInfo));
+});
+
 // Utilities
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,6 +35,3 @@ app.set('port', process.env.PORT || 3000);
 server.listen(app.get('port'), () => {
   console.log('Express server listening on port: ', app.get('port'));
 });
-
-// app.listen(3000, () => console.log('Listening on port ', port));
-// server.listen(3000, () => console.log("listening"));
