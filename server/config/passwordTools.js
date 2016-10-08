df05223a11477976516678aadc49f06b462c4cbb
Michael <michaelhuytran@gmail.com>
2016-10-08 13:03:14 -0700

    [feature] - password encryption

--- server/config/passwordTools.js
+++ server/config/passwordTools.js
@@ -2,6 +2,7 @@ const pbkdf2 = require('pbkdf2');
 var crypto = require('crypto');
 var salt = crypto.randomBytes(128).toString('base64');
 
+
 hash = param => {
   return new Promise(function(resolve, reject) {
     crypto.pbkdf2(param, salt, 10000, 512, function(error, hash) {
@@ -27,4 +28,3 @@ module.exports = {
   hash,
   checkPassword
 }
-
