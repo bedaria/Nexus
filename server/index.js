40f3272ba58ab60d9ea2b00cba5ce4bb23af81a1
Sam Chi <samuel.sj.chi@gmail.com>
2016-10-08 09:33:06 -0700

    [setup] - sets up Express server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
-'use strict';
+const express    = require('express');
+const path       = require('path');
+const logger     = require('morgan')
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+const db         = require('./config/db.js');
+const app        = express();
 require('./models/models.js');
-const db = ('.config/db.js');
 
-const express = require('express');
-const port = process.env.PORT || 3000;
+// Utilities
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../src')));
 
-const app = express();
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/../src/index.html'));
+});
 
-app.listen(port, () => {
-    console.log('Listening on: http://localhost: ', port);
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port: ', app.get('port'));
 });
