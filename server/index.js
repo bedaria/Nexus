a19e3176f1a7e3b4f3a506cec92aa40d36c5b9b6
bedaria <dburinsk@gmail.com>
2016-10-08 10:20:07 -0700

    [pull] - adds socket.io to backend

--- server/index.js
+++ server/index.js
@@ -2,10 +2,24 @@
 require('./models/models.js');
 
 const express = require('express');
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 4200;
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.listen(port, () => {
-    console.log('Listening on: http://localhost: ', port);
+app.get('/api/hello', (req, resp) => {
+  resp.send("hellor")
+})
+
+io.on('connection', socket => {
+  console.log("user connected")
+
+  socket.on('disconnect', () => console.log("user disconnected"));
+  socket.on('announce', (msg) => {
+    console.log("received message! ", msg)
+    io.emit('a', msg)
+  })
 });
+
+// app.listen(3000, () => console.log('Listening on port ', port));
+server.listen(3000, () => console.log("listening"));
