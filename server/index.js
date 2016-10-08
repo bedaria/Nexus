be5c024cc35b989d6c71e436589864cc86f60875
bedaria <dburinsk@gmail.com>
2016-10-08 10:23:24 -0700

    [fix] - fixes static file path

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, '/../src')));
 
 // Routing
 app.use('/api', adminRoutes);
