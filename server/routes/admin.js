cd103093a11a9d2d2e26756ef6f1055626f2e31a
Sam Chi <samuel.sj.chi@gmail.com>
2016-10-22 22:31:55 -0700

    [refactor] - reorganizes admin routes for todo list operations

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,25 +1,17 @@
-const router            = require('express').Router();
-
-const path              = require('path');
-const controller        = require('../controllers/admin');
-
-router.post('/admin/users', (req, res) => {
-  res.status(200).send('POST /api/admin/users');
-});
+const router     = require('express').Router();
+const path       = require('path');
+const controller = require('../controllers/admin');
 
 /* ------------------- TO DO LIST ------------------- */
-router.post('/admin/users/:userId/todos', controller.todos.add);
-router.put('/admin/users/:userId/todos/:todoId', controller.todos.update);
-router.delete('/admin/users/:userId/todos/:todoId', controller.todos.delete);
-router.get('/admin/users/:userId/todos', controller.todos.fetchAll);
+router.get('/admin/user/todos', controller.todos.fetchAll);
+router.post('/admin/user/todos', controller.todos.add);
+router.delete('/admin/user/todos/:todoId', controller.todos.delete);
 
 /* ------------------- USER SIGNUP + LOGIN ------------------- */
-
 router.post('/admin/user/signup', controller.auth.signUp);
 router.post('/admin/user/signin', controller.auth.signIn);
 
 /* 404 Redirection */
-router.get('*', (req, res) => res.sendStatus(404) );
-
+router.get('*', (req, res) => res.sendStatus(404));
 
 module.exports = router;
